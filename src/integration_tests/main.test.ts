3ecc39f360e4b4056bcfab9a3ff5b490061f24b5
Gus Narea <gnarea@users.noreply.github.com>
2021-03-17 12:03:22 +0000

    Revert "chore: Temporarily skip integration test"
    This reverts commit 997fccee

--- src/integration_tests/main.test.ts
+++ src/integration_tests/main.test.ts
@@ -1,7 +1,6 @@
 import { deliverParcel } from '..';
 
-// TODO: Reinstate
-test.skip('Real public gateway should refuse malformed parcel', async () => {
+test('Real public gateway should refuse malformed parcel', async () => {
   await expect(
     deliverParcel('https://frankfurt.relaycorp.cloud', Buffer.from('hey')),
   ).rejects.toMatchObject(expect.objectContaining({ message: expect.stringMatching('HTTP 400') }));
