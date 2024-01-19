9ebb94a534d01619476dc0a347e86bbb09175fdc
Gus Narea <gnarea@users.noreply.github.com>
2024-01-19 15:31:25 +0000

    test: Use new Belgian gateway in functional test

--- src/integration_tests/main.test.ts
+++ src/integration_tests/main.test.ts
@@ -2,6 +2,6 @@ import { deliverParcel } from '..';
 
 test('Real public gateway should refuse malformed parcel', async () => {
   await expect(
-    deliverParcel('frankfurt.relaycorp.cloud', Buffer.from('hey')),
+    deliverParcel('belgium.relaycorp.services', Buffer.from('hey')),
   ).rejects.toMatchObject(expect.objectContaining({ message: expect.stringMatching('HTTP 400') }));
 });
